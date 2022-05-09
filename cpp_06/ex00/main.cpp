6129066d0b39d6c6b06d29727467ffa90fdd4b15
punkertron <vascherbatov@mail.ru>
2022-05-09 21:36:03 +0300

    done ex00

--- cpp_06/ex00/main.cpp
+++ cpp_06/ex00/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <limits>
 #include <cstdlib>
+#include <cmath>
 
 enum e_begin
 {
@@ -28,11 +29,31 @@ static void	printBegin(e_begin a)
 	}
 }
 
+static bool	charOverflow(const double &a)
+{
+	return (a > std::numeric_limits<char>::max() || a < std::numeric_limits<char>::min());
+}
 
+static bool	intOverflow(const double &a)
+{
+	return (a > std::numeric_limits<int>::max() || a < std::numeric_limits<int>::min());
+}
+
+static bool isNumber(const double &a)
+{
+	return (!std::isnan(a) && std::isfinite(a));
+}
+
+static bool	checkNanInf(const std::string &a)
+{
+	return (a.find("nan") != std::string::npos || a.find("inf") != std::string::npos);
+}
 
 static	void charConvert(const char &c)
 {
 	printBegin(CHAR_P);
+	if (charOverflow(static_cast<double>(c)))
+		std::cout << "impossible\n";
 	if (!isprint(c))
 		std::cout << "Non displayable\n";
 	else
@@ -45,13 +66,74 @@ static	void charConvert(const char &c)
 	std::cout << static_cast<double>(c) << std::endl;
 }
 
+
 static void	floatConvert(const float &f)
 {
 	printBegin(CHAR_P);
-	if (f > std::numeric_limits<char>::max() || f < std::numeric_limits<char>::min())
+	if (!isNumber(f) || charOverflow(static_cast<double>(f)))
 		std::cout << "impossible\n";
 	else if (!isprint(f))
 		std::cout << "Non displayable\n";
+	else
+		std::cout << "'" << static_cast<char>(f) << "'" << std::endl;
+	
+	printBegin(INT_P);
+	if (!isNumber(static_cast<double>(f)) || intOverflow(static_cast<double>(f)))
+		std::cout << "impossible\n";
+	else
+		std::cout << static_cast<int>(f) << std::endl;
+
+	printBegin(FLOAT_P);
+	std::cout << f << 'f' << std::endl;
+	printBegin(DOUBLE_P);
+	std::cout << static_cast<double>(f) << std::endl;
+}
+
+
+static void	doubleConvert(const double &d)
+{
+	printBegin(CHAR_P);
+	if (charOverflow(d))
+		std::cout << "impossible\n";
+	else if (!isprint(d))
+		std::cout << "Non displayable\n";
+	else
+		std::cout << "'" << static_cast<char>(d) << "'" << std::endl;
+	
+
+	printBegin(INT_P);
+	if (intOverflow(d) || !isNumber(d))
+		std::cout << "impossible\n";
+	else
+		std::cout << static_cast<int>(d) << std::endl;
+
+	printBegin(FLOAT_P);
+	std::cout << static_cast<float>(d) << 'f' << std::endl;
+	printBegin(DOUBLE_P);
+	std::cout << d << std::endl;
+}
+
+
+static void intConvert(const long &i)
+{
+	printBegin(CHAR_P);
+	if (charOverflow(static_cast<double>(i)))
+		std::cout << "impossible\n";
+	else if (!isprint(i))
+		std::cout << "Non displayable\n";
+	else
+		std::cout << "'" << static_cast<char>(i) << "'" << std::endl;
+	
+	printBegin(INT_P);
+	if (intOverflow(static_cast<double>(i)))
+		std::cout << "impossible\n";
+	else
+		std::cout << static_cast<int>(i) << std::endl;
+
+	printBegin(FLOAT_P);
+	std::cout << static_cast<float>(i) << 'f' << std::endl;
+	printBegin(DOUBLE_P);
+	std::cout << static_cast<double>(i) << std::endl;;
 }
 
 int main(int argc, char **argv)
@@ -64,10 +146,14 @@ int main(int argc, char **argv)
 	std::string	input = argv[1];
 	std::cout << std::setprecision(1) << std::fixed;
 
-	if (input[0] == '\'')
-		charConvert(static_cast<char> (input[1]));
-	if (input.find('f') != std::string::npos)
-		floatConvert(static_cast<float>((std::strtof(input.c_str(), NULL))));
 
+	if (input[0] == '\'' && !checkNanInf(input))
+		charConvert(static_cast<char> (input[1]));
+	else if (input.find('f') != std::string::npos || checkNanInf(input))
+		floatConvert(static_cast<float>(std::strtof(input.c_str(), NULL)));
+	else if (input.find('.') != std::string::npos)
+		doubleConvert(static_cast<double>(std::strtod(input.c_str(), NULL)));
+	else
+		intConvert(static_cast<long>(std::strtol(input.c_str(), NULL, 10)));
 	return (0);
 }
